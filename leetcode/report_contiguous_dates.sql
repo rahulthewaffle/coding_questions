c7b7d8c7192104c87ed0d44dc86cd21e5b5333db
Rahul Devanarayanan <rahul.sairam@gmail.com>
2021-09-30 04:20:54 -0700

    Bugfixed end date calculation in contiguous dates

--- leetcode/report_contiguous_dates.sql
+++ leetcode/report_contiguous_dates.sql
@@ -5,28 +5,35 @@ with
         select
             'succeeded' as period_state,
             success_date as period_date,
-            row_number() over (partition by success_date order by success_date desc) as rownum
+            row_number() over (order by success_date asc) as rownum
         from succeeded
-        where success_date between date('2019-01-01', '%Y-%m-%d') and date('2019-12-31', '%Y-%m-%d')
+        where success_date between str_to_date('2019-01-01', '%Y-%m-%d') and str_to_date('2019-12-31', '%Y-%m-%d')
         union
         select
             'failed' as period_state,
             fail_date as period_date,
-            row_number() over (partition by success_date order by success_date desc) as rownum
+            row_number() over (order by fail_date asc) as rownum
         from failed
-        where fail_date between date('2019-01-01', '%Y-%m-%d') and date('2019-12-31', '%Y-%m-%d')
+        where fail_date between str_to_date('2019-01-01', '%Y-%m-%d') and str_to_date('2019-12-31', '%Y-%m-%d')
     ),
-    start_dates as (
+    grouping_dates as (
         select
             period_state,
-            dateadd('day', rownum, period_date) as start_date,
-            count(*) as period_count
+            period_date,
+            date_add(period_date, interval -rownum day) as grouping_date,
         from raw_periods
-        group by period_state, dateadd('day', rownum, period_date)
+    ),
+    start_dates as(
+        select
+            period_state,
+            min(period_date) as start_date,
+            count(*)
+        from grouping_dates
+        group by grouping_date, period_state
     )
 select
     period_state,
     start_date,
-    dateadd('day', period_count, start_date) as end_date
+    date_add(start_date, interval period_count-1 day) as end_date
 from start_dates
 order by start_date asc
